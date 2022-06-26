0825e7265994ed7ebf5387f483c64b1923be576e
jimzer <jimi.vaubien@protonmail.com>
2022-06-26 19:02:01 +0200

    Nix shell to manage node version and easy development

---
+++ shell.nix
@@ -0,0 +1,12 @@
+with import <nixpkgs> {};
+
+stdenv.mkDerivation {
+    name = "node";
+    buildInputs = [
+        nodejs-16_x
+        (yarn.override { nodejs = nodejs-16_x ;})
+    ];
+    shellHook = ''
+        export PATH="$PWD/node_modules/.bin/:$PATH"
+    '';
+}
