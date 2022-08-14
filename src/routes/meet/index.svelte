277f6b2d0aaeaf29816da7c5cdb56e6b57642543
francescodisalvo05 <francesco.disalvo99@gmail.com>
2022-08-14 16:25:45 +0200

    Add main skeleton /meet

---
+++ src/routes/meet/index.svelte
@@ -0,0 +1,63 @@
+<script lang="ts">
+</script>
+
+<div class="grid grid-cols-1 justify-between"> <!-- generalize! -->
+
+    <div class="text-xl m-auto w-max h-max mb-32" 
+    style="margin:15vh">
+        
+        <!-- adjust text ? -->
+        <p style="text-color:rgb(26 30 59)"><b>Have you always wanted to exchange opinions about Artificial Intelligence?</b></p>
+        <p>You're in the right place then!</p> 
+        
+        <br />
+
+        <p>
+            In January 2022, after years of University spent wishing to find a place
+            for sharing ideas, we wondered if it was the right time for us to create our community. <br />
+            Hence, stAI tuned was created: a place where everyone can share his own knowledge and research projects. <br /><br />
+
+            Nowadays Artificial Intelligence became a buzzword, and you can hear it wherever you go. <br />
+        </p>
+
+        <br />
+
+        <p style="text-color:rgb(26 30 59)"><b>But how many people are really conscious about it?</b></p>
+        <p>Our goal is make Artificial Intelligence accessible to anyone!</p>
+
+
+
+
+    </div>
+
+
+</div>
+
+<section class="">
+    <div class="h-[400px] w-[800px] bg-primary-600 motto text-center text-2xl font-bold text-white content-center">
+        <p><br /><br /><br />This is our motto.</p>
+    </div>
+</section>
+
+<!-- roadmap !! -->
+
+
+<section class="mt-20 mb-20">
+
+    <p class="text-5xl font-bold text-primary-600 uppercase text-center">Our team</p>
+
+    <div class="grid grid-cols-3 justify-between flex text-center m-8 w-full justify-items-center">
+
+        <div class="team-card shadow-xl">
+            <center><div class="button-team text-2xl font-bold text-white">TECH</div></center>
+        </div>
+
+        <div class="team-card">
+            <center><div class="button-team text-2xl font-bold text-white">MARKETING</div></center>
+        </div>
+
+        <div class="team-card">
+            <center><div class="button-team text-2xl font-bold text-white">WRITERS</div></center>
+        </div>
+    </div>
+</section>
