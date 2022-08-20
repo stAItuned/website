b61d3f6336a012d24c89176e320baac4c02cf2a0
francescodisalvo05 <francesco.disalvo99@gmail.com>
2022-08-20 19:04:11 +0200

    Fix spacing

--- src/features/blog/components/card.svelte
+++ src/features/blog/components/card.svelte
@@ -4,9 +4,10 @@
 	export let articleMeta: ArticleMeta
 </script>
 
+
 <a class="text-3xl" href="/learn/{articleMeta.slug}">
 	<div
-		class="flex flex-col rounded-md shadow-lg hover:cursor-pointer w-[400px] h-[400px] bg-bgLearnCard bg-no-repeat bg-[length:200%_200%] bg-center"
+		class="flex flex-col rounded-md shadow-lg hover:cursor-pointer w-[400px] h-[450px] bg-bgLearnCard bg-no-repeat bg-[length:200%_200%] bg-center"
 	>
 		<div class="h-[calc(40%+100px)] mt-auto">
 			<div class="h-[100px] w-[50px] rounded-bl-[50px] shadow-[0_50px_0_0_#FFF272]" />
@@ -31,23 +32,3 @@
 		</div>
 	</div>
 </a>
-
-<!-- <a class="text-3xl" href="/blog/{articleMeta.slug}">
-	<div
-		class="flex flex-col gap-4 rounded-md shadow-lg p-16 hover:bg-slate-500 hover:cursor-pointer w-[400px] h-[300px]"
-	>
-		<p>
-			{articleMeta.title}
-		</p>
-
-		<div class="flex gap-4">
-			{#each articleMeta.tags as tag}
-				<p class="text-sm rounded-lg bg-blue-100 p-1">{tag}</p>
-			{/each}
-		</div>
-
-		<p class="text-sm line-clamp-3">
-			{articleMeta.description}
-		</p>
-	</div>
-</a> -->
