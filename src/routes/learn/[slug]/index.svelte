474e1b1c2e9eaf8b073cdd2dc287e15b7df241e4
francescodisalvo05 <francesco.disalvo99@gmail.com>
2022-08-20 19:03:59 +0200

    Render Latex

--- src/routes/learn/[slug]/index.svelte
+++ src/routes/learn/[slug]/index.svelte
@@ -3,10 +3,40 @@
 
 	// populated with data from the endpoint
 	export let article: Article
+
+	import { onMount } from 'svelte';
+
+	let equ1 = `$$\\quad$$`;
+
+	onMount(() => {
+		
+		let script = document.createElement('script');
+    script.src = "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-svg.js";
+    document.head.append(script);
+		
+		script.onload = () => {
+      MathJax = {
+        tex: {inlineMath: [['$', '$'], ['\\(', '\\)']]},
+        svg: {fontCache: 'global'}
+      };
+		};
+					  
+	});
 </script>
 
+
+
 <article class="prose prose-xl m-auto">
-	<h1 class="text-6xl w-max m-auto">{article.meta.title.trim()}</h1>
+	<!-- the title is already inside the article -->
+		
+	<!--
+	<h1 class="text-4xl w-auto m-auto tracking-wide">
+		{article.meta.title.trim()}
+	</h1> -->
+
+	<!-- it doesn't render latex from notion the first run without an already rendered element
+		 Workaround: display a "tab" in latex -->
+	<!-- {equ1} -->
 
 	{@html article.parsedContent}
 </article>
