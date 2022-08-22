b4789a76fe55b1b6acdfe151cf4b83928b54f2e0
francescodisalvo05 <francesco.disalvo99@gmail.com>
2022-08-22 15:24:17 +0200

    Update alt text images

--- src/features/header/components/footer.svelte
+++ src/features/header/components/footer.svelte
@@ -6,16 +6,16 @@
 	class="flex shadow-xl px-16 py-4 rounded-tl-[30px] bg-primary-600 w-full h-auto flex items-center grid lg:grid-cols-3 md:grid-cols-3 sm:grid-cols-1"
 >
 
-    <div class="grid grid-cols-3 bg-primary-600 pb-4 uppercase ">
+    <div class="grid grid-cols-3 bg-primary-600 pb-4 uppercase text-white text-sm">
         
-        <div class="self-end text-xl font-bold text-stayYellow-600 pt-4 bg-themes social">
-			<center><img src="assets/general/ig-logo.png" alt="ig-logo"/></center>
+        <div class="self-end pt-4 bg-themes social">
+			<center><img src="static/assets/general/ig-logo.png" alt="ig-logo"/></center>
 		</div>
-        <div class="self-end text-xl font-bold text-stayYellow-600 pt-4 bg-themes social">
-			<center><img src="assets/general/ig-logo.png" alt="ig-logo"/></center>
+        <div class="self-end pt-4 bg-themes social">
+			<center><img src="static/assets/general/ig-logo.png" alt="ig-logo"/></center>
 		</div>
-        <div class="self-end text-xl font-bold text-stayYellow-600 pt-4 bg-themes social">
-			<center><img src="assets/general/ig-logo.png" alt="ig-logo"/></center>
+        <div class="self-end pt-4 bg-themes social">
+			<center><img src="static/assets/general/ig-logo.png" alt="ig-logo"/></center>
 		</div>
 
     </div>
