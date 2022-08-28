20e8192b076ada96a203e7c8b071188da092bc29
francescodisalvo05 <francesco.disalvo99@gmail.com>
2022-08-28 20:16:50 +0200

    Add filters to /learn

--- src/routes/learn/index.svelte
+++ src/routes/learn/index.svelte
@@ -1,14 +1,17 @@
 <script lang="ts">
 	import type { ArticleMeta } from '@lib/notion'
 	import { BlogCard } from '@features/blog/index.ts'
-	
+	import {Filters } from '@features/header/index.ts'
+
 	// populated with data from the endpoint
 	export let articleMetas: ArticleMeta[]
 
 	
 </script>
 
-	<h1 class="text-5xl m-auto w-max mb-32">stAI Blog</h1>
+
+
+	<Filters />
 
 	<center class="mb-32">
 		<div class="flex w-max sm:m-2px md:m-auto grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-4 2xl:grid-cols-5">
