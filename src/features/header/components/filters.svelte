211016d7e0b9f2981853ac29821c69319a34e029
francescodisalvo05 <francesco.disalvo99@gmail.com>
2022-08-28 20:15:15 +0200

    Add filters component

---
+++ src/features/header/components/filters.svelte
@@ -0,0 +1,25 @@
+<script>
+	import HamburgerFilters from './hamburger_filters.svelte'
+    import SidebarFilters from './sidebar_filters.svelte'
+	
+	export let open_filters = false
+    
+</script>
+
+<div class="grid grid-cols-5">
+		
+    <input type="text" class="
+                mt-10 mb-10 m-auto
+                col-span-4
+                block
+                w-4/6
+                rounded-xl
+                shadow-xl" placeholder="  Search">
+
+
+    <HamburgerFilters bind:open_filters={open_filters}/>
+
+    <SidebarFilters bind:open_filters/>
+
+
+</div>
---
+++ src/features/header/components/hamburger_filters.svelte
@@ -0,0 +1,44 @@
+<script>
+	export let open_filters = false
+</script>
+
+<button class="z-10 text-[#1A1E3B] hover:text-[#1A1E3B] cursor-pointer mr-4 border-none focus:outline-none" class:open_filters on:click={() => open_filters = !open_filters}>
+	<svg width=32 height=24>
+		<line id="top" x1=0 y1=2  x2=32 y2=2/>
+		<line id="middle" x1=12 y1=12 x2=32 y2=12/>
+		<line id="bottom" x1=0 y1=22 x2=32 y2=22/>
+	</svg>
+</button>
+
+<style>
+	svg {
+		min-height: 24px;
+		transition: transform 0.3s ease-in-out;
+	}
+	
+	svg line {
+		stroke: currentColor;
+		stroke-width: 3;
+		transition: transform 0.3s ease-in-out
+	}
+	
+	button {
+		z-index: 10;
+	}
+	
+	.open_filters svg {
+		transform: scale(0.7)
+	}
+	
+	.open_filters #top {
+		transform: translate(6px, 0px) rotate(45deg)
+	}
+	
+	.open_filters #middle {
+		opacity: 0;
+	}
+	
+    .open_filters #bottom {
+		transform: translate(-12px, 9px) rotate(-45deg)
+	}
+</style>
