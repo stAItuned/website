e89ed3a39be8676bdd5628e857b374a6a8a2fe68
francescodisalvo05 <francesco.disalvo99@gmail.com>
2022-08-28 20:16:03 +0200

    Add components

--- src/features/header/index.ts
+++ src/features/header/index.ts
@@ -3,3 +3,8 @@ export { default as Footer } from './components/footer.svelte'
 
 export { default as HeaderResponsive } from './components/header_responsive.svelte'
 export { default as Sidebar } from './components/sidebar.svelte'
+
+export { default as Filters } from './components/filters.svelte'
+export { default as SidebarFilters } from './components/sidebar_filters.svelte'
+
+
