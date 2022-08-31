78bf84b8eacd70564f1ec193b3cd64f7141df5f5
francescodisalvo05 <francesco.disalvo99@gmail.com>
2022-08-31 21:56:13 +0200

    Update icons and logo

--- src/features/header/components/footer.svelte
+++ src/features/header/components/footer.svelte
@@ -3,28 +3,30 @@
 </script>
 
 <footer
-	class="flex shadow-xl px-16 py-4 rounded-tl-[30px] bg-primary-600 w-full h-auto flex items-center grid lg:grid-cols-3 md:grid-cols-3 sm:grid-cols-1"
+	class="flex shadow-xl px-16 py-4 rounded-tl-[30px] bg-primary-600 w-full h-auto flex items-center grid grid-cols-3 md:grid-cols-3 sm:grid-cols-1"
 >
 
-    <div class="grid grid-cols-3 bg-primary-600 pb-4 uppercase text-white text-sm">
+    <div class="bg-primary-600 pb-4 uppercase text-white text-sm">
         
-        <div class="self-end pt-4 bg-themes social">
-			<center><img src="assets/general/ig-logo.png" alt="ig-logo"/></center>
-		</div>
-        <div class="self-end pt-4 bg-themes social">
-			<center><img src="assets/general/ig-logo.png" alt="ig-logo"/></center>
-		</div>
-        <div class="self-end pt-4 bg-themes social">
-			<center><img src="assets/general/ig-logo.png" alt="ig-logo"/></center>
-		</div>
-
+        <center>
+            <div class="items-center pt-4 bg-themes social flex place-content-center">
+                <a href="https://www.linkedin.com/company/stai-tuned/" target="_blank" class="mr-1">
+                    <img src="assets/footer/LinkedIn.svg" alt="ig-logo"/>
+                </a>
+                <a href="https://www.linkedin.com/company/stai-tuned/" target="_blank" class="ml-1">
+                    <img src="assets/footer/Instagram.svg" alt="ig-logo"/>
+                </a>
+                
+            </div>
+        </center>
     </div>
 
     <div>
-	    <p class="text-center text-3xl lg:text-4xl font-bold text-stayYellow-600 ">JOIN US</p>
+	    <!--<p class="text-center text-3xl lg:text-4xl font-bold text-stayYellow-600" style="text-shadow: 3px 3px #ffE700">JOIN US</p>-->
+        <center><a href="/join"><img class=""  src="assets/footer/JoinUS.svg" alt="JoinUS" /></a></center>
         <p class="text-center text-s font-bold text-white ">Copyright ©stAItuned</p>
     </div>
     
-    <p class="text-center text-xl font-bold text-white uppercase" style="padding:15px 0"> Contacts </p>
+    <p class="text-center text-xl font-bold text-white uppercase" style="padding:15px 0"> Keep in touch </p>
 
 </footer>
