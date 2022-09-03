b70e4c1af0e689a6923ccb89797cfafdab503571
ExalFabu <53974096+ExalFabu@users.noreply.github.com>
2022-09-04 00:04:01 +0200

    bugfix: footer imgs href are now absolute

--- src/features/header/components/footer.svelte
+++ src/features/header/components/footer.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { CONFIG } from '@config'
-</script>
+	</script>
 
 <footer
 	class="flex shadow-xl px-16 py-4 rounded-tl-[30px] bg-primary-600 w-full h-auto flex items-center grid grid-cols-3 md:grid-cols-3 sm:grid-cols-1"
@@ -11,10 +11,10 @@
         <center>
             <div class="items-center pt-4 bg-themes social flex place-content-center">
                 <a href="https://www.linkedin.com/company/stai-tuned/" target="_blank" class="mr-1">
-                    <img src="assets/footer/LinkedIn.svg" alt="ig-logo"/>
+                    <img src="/assets/footer/LinkedIn.svg" alt="ig-logo"/>
                 </a>
                 <a href="https://www.linkedin.com/company/stai-tuned/" target="_blank" class="ml-1">
-                    <img src="assets/footer/Instagram.svg" alt="ig-logo"/>
+                    <img src="/assets/footer/Instagram.svg" alt="ig-logo"/>
                 </a>
                 
             </div>
@@ -23,7 +23,7 @@
 
     <div>
 	    <!--<p class="text-center text-3xl lg:text-4xl font-bold text-stayYellow-600" style="text-shadow: 3px 3px #ffE700">JOIN US</p>-->
-        <center><a href="/join"><img class=""  src="assets/footer/JoinUS.svg" alt="JoinUS" /></a></center>
+        <center><a href="/join"><img class=""  src="/assets/footer/JoinUS.svg" alt="JoinUS" /></a></center>
         <p class="text-center text-s font-bold text-white ">Copyright ©stAItuned</p>
     </div>
     
