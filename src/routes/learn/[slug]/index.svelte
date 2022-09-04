d6e7584197dd5879e7a46295e28381ac855400ec
ExalFabu <53974096+ExalFabu@users.noreply.github.com>
2022-09-04 15:35:49 +0200

    share animation into clipboard icon

--- src/routes/learn/[slug]/index.svelte
+++ src/routes/learn/[slug]/index.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
 	import type { Article } from '@lib/notion'
+	import { draw } from 'svelte/transition'
+	import { sineIn } from 'svelte/easing'
+	import { onMount } from 'svelte'
 
 	// populated with data from the endpoint
 	export let article: Article
 
-	import { onMount } from 'svelte'
-
-	let equ1 = `$$\\quad$$`
+	// allows the little animation when sharing
+	let shared = false
 
 	onMount(() => {
 		let script = document.createElement('script')
@@ -26,8 +28,12 @@
 			}
 		}
 	})
-	
-	const localtime = (new Date(article.meta.publishDate)).toLocaleDateString(undefined, { year: '2-digit', month: 'long', day: 'numeric' })
+
+	const localtime = new Date(article.meta.publishDate).toLocaleDateString(undefined, {
+		year: '2-digit',
+		month: 'long',
+		day: 'numeric'
+	})
 
 	const share = async () => {
 		const shareData = {
@@ -37,18 +43,23 @@
 		}
 		try {
 			await navigator.share(shareData)
-		} catch (err){
-		    // TODO: Alternative to navigator.share
-			// i was thinking of copying the url and show a toast or a popup saying that
-			// or animating the icon to a clipboard? maybe this is easier
-			console.log("Share failed")
-			console.log(err)
+		} catch (err) {
+			// console.log('navigation.share failed, fallback')
+			try {
+				await navigator.clipboard.writeText(window.location.href)
+			} catch {
+				// console.log('not even able to copy into clipboard')
+				return
+			}
+			shared = true
+			setTimeout(() => {
+				shared = false
+			}, 3000)
 		}
 	}
 </script>
 
 <article class="prose prose-xl text-sm md:text-md lg:text-lg mt-10 mb-20 px-4 mx-auto">
-
 	<div
 		class="grid justify-items-center mb-2"
 		style="column-gap: 1em; grid-template-columns: minmax(min-content, 1fr) max-content minmax(min-content, 1fr) min-content"
@@ -106,18 +117,41 @@
 			{article.meta.readingTime}m
 		</div>
 		<div class="article-metadata-item">
-			<button on:click={share}>
+			{#if shared}
 				<svg
 					xmlns="http://www.w3.org/2000/svg"
-					viewBox="0 0 20 20"
-					fill="currentColor"
-					class="w-5 h-5"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke-width="1.5"
+					stroke="currentColor"
+					class="w-6 h-6"
 				>
 					<path
-						d="M13 4.5a2.5 2.5 0 11.702 1.737L6.97 9.604a2.518 2.518 0 010 .792l6.733 3.367a2.5 2.5 0 11-.671 1.341l-6.733-3.367a2.5 2.5 0 110-3.475l6.733-3.366A2.52 2.52 0 0113 4.5z"
+						in:draw={{ duration: 500, easing: sineIn }}
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						d="M11.35 3.836c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m8.9-4.414c.376.023.75.05 1.124.08 1.131.094 1.976 1.057 1.976 2.192V16.5A2.25 2.25 0 0118 18.75h-2.25m-7.5-10.5H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V18.75m-7.5-10.5h6.375c.621 0 1.125.504 1.125 1.125v9.375m-8.25-3l1.5 1.5 3-3.75"
 					/>
 				</svg>
-			</button>
+			{:else}
+				<button on:click={share}>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke-width="1.5"
+						stroke="currentColor"
+						class="w-6 h-6"
+					>
+						<path
+							in:draw={{ duration: 1000, easing: sineIn }}
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							d="M7.217 10.907a2.25 2.25 0 100 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186l9.566-5.314m-9.566 7.5l9.566 5.314m0 0a2.25 2.25 0 103.935 2.186 2.25 2.25 0 00-3.935-2.186zm0-12.814a2.25 2.25 0 103.933-2.185 2.25 2.25 0 00-3.933 2.185z"
+						/>
+					</svg>
+				</button>
+			{/if}
 		</div>
 	</div>
 	{@html article.parsedContent}
