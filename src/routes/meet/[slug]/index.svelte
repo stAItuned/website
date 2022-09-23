39e813039188194be4004bda4e5bcdf61db5a701
francescodisalvo05 <francesco.disalvo99@gmail.com>
2022-09-23 13:30:18 +0200

    Update margins on Team cards

--- src/routes/meet/[slug]/index.svelte
+++ src/routes/meet/[slug]/index.svelte
@@ -5,10 +5,10 @@
 <h1 class="text-bold text-5xl m-auto mt-20 w-max mb-20 uppercase">slug - team</h1>
 
 
-		<div class="px-4 flex flex-wrap justify-center">
-            
-            <!-- Iterate for each collaborator and give its data as input-->
-			<TeamCard  />
-            <TeamCard  />
+<div class="px-4 flex flex-wrap justify-center m-20">
+    
+    <!-- Iterate for each collaborator and give its data as input-->
+    <TeamCard  />
+    <TeamCard  />
 
-		</div>
+</div>
