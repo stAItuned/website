eaa7190d1e81d067576eb3f8e805804736102b09
francescodisalvo05 <francesco.disalvo99@gmail.com>
2022-09-24 10:45:17 +0200

    Add team cards and integrate backend

--- src/routes/meet/[slug]/+page.svelte
+++ src/routes/meet/[slug]/+page.svelte
@@ -1,14 +1,36 @@
 <script lang="ts">
     import { TeamCard } from '@features/blog/index'
+
+    import type { Author } from '@lib/git/types'
+    import type { PageData } from '.svelte-kit/types/src/routes/meet/[slug]/$types'
+
+    import { page } from '$app/stores';
+
+    export let data: PageData
+	const authors: Author[] = data.authors
+
+    let slug = $page.url.pathname.split("/")[2]
+    const capitalizedSlug = slug.charAt(0).toUpperCase() + slug.slice(1);
+
+    
+
+    const filterAuthors = authors.filter(author => author.team.indexOf(capitalizedSlug) > -1)
+
+
+
 </script>
 
-<h1 class="text-bold text-5xl m-auto mt-20 w-max mb-20 uppercase">slug - team</h1>
 
+<div class="min-h-screen">
 
-<div class="px-4 flex flex-wrap justify-center m-20">
-    
-    <!-- Iterate for each collaborator and give its data as input-->
-    <TeamCard  />
-    <TeamCard  />
+    <h1 class="text-bold text-5xl m-auto mt-20 w-max mb-20 uppercase">{capitalizedSlug} team</h1>
+
+    <div class="px-4 flex flex-wrap justify-center m-20">
+
+        {#each filterAuthors as author}
+            <TeamCard  {author} />
+        {/each}
+
+    </div>
 
 </div>
