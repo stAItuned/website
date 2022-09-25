a4c57b97cdbc504a812be6bf094b2b7d69318ada
ExalFabu <53974096+ExalFabu@users.noreply.github.com>
2022-09-25 23:16:38 +0200

    basic error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+</script>
+
+<div class="h-[80vh] m-4 text-center">
+	<h1 class="text-xl">Oops... Something went wrong</h1>
+	<p>Looks like there's been a mistake, either on your side or on our side. We're sorry :(</p>
+	<p>
+		<a href="/" class="underline">Go back to the homepage</a>
+	</p>
+</div>
