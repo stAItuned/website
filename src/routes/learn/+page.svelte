0aaf33f10b7e0077770495dc4a0dc09f15971d57
Andrea Deluca <79919899+andrea-deluca@users.noreply.github.com>
2022-10-30 21:46:38 +0100

    Assets in target cards added

--- src/routes/learn/+page.svelte
+++ src/routes/learn/+page.svelte
@@ -1,4 +1,9 @@
 <script lang="ts">
+	// @ts-ignore
+	import newbieCard from '@assets/learn/newbie-card.png?w=600?webp'
+	// @ts-ignore
+	import expertCard from '@assets/learn/expert-card.png?w=600?webp'
+
 	import { page } from '$app/stores'
 
 	import { Filters } from '@lib/configs'
@@ -13,9 +18,14 @@
 		<Breadcrumb tabs={utils.getTabsFromPathname(pathname)} />
 		<div class="flex lg:flex-row flex-col justify-between gap-16">
 			{#each Filters.TARGETS as target}
-				<div class="shadow w-full flex flex-col justify-end h-[400px] bg-slate-200 rounded-lg p-16">
-					<a href={`/learn/${target.toLowerCase()}`}>
-						<Button rounded="full" width="full" className="text-3xl py-6">{target}</Button>
+				<div class="w-full px-8">
+					<a href={`/learn/${target.toLowerCase()}`} class="items-center flex flex-col">
+						<img
+							srcset={target === 'Newbie' ? newbieCard : expertCard}
+							class="w-64 h-64 hover:scale-125 transition"
+							alt="target card"
+						/>
+						<Button rounded="full" width="full" className="text-3xl py-6 z-10">{target}</Button>
 					</a>
 				</div>
 			{/each}
