94ba4445ba597f6db2482a338011325978eda376
francescodisalvo05 <francesco.disalvo99@gmail.com>
2022-11-28 07:44:28 +0100

    Update relevant articles

--- src/lib/configs/home.ts
+++ src/lib/configs/home.ts
@@ -5,6 +5,10 @@ export const ARTICLE_CARD_COLORS = ['#ffb347', '#a7c7e7', '#47efff', '#77dd77'] 
 export const AMOUNT_OF_ARTICLES = 6 as const
 
 export const RELEVANT_ARTICLES_SLUG = [
-    'cos-è-un-algoritmo-e-per-cosa-si-utilizza',
-    'cos-è-un-computer'
+    'model-uncertainty-through-monte-carlo-dropout-pt1',
+    'study-case-the-last-of-us',
+    'assortativity-coefficients-in-social-network-data',
+    'five-reasons-why-my-machine-learning-models-failed',
+    'super-resolution-what-is-it-and-why-is-it-useful',
+    '30-days-of-machine-learning-engineering'
 ] as const 
