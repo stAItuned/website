8fe04260400f4ef6165363a735ce7ae515b92648
Javier Gongora <42308135+javigong@users.noreply.github.com>
2023-02-25 15:29:59 -0800

    Get related articles, filtering by target and topic

--- src/routes/learn/[target]/[slug]/+page.server.ts
+++ src/routes/learn/[target]/[slug]/+page.server.ts
@@ -1,15 +1,36 @@
-import type { PageServerLoad } from './$types'
 import { error } from '@sveltejs/kit'
+import type { PageServerLoad } from './$types'
 
 import { git } from '@lib/git'
 
 export const load: PageServerLoad = async ({ params }) => {
-    const articles = (await git.loadData()).articles
-    const article = articles.find((article) => article.slug === params.slug)
-    if (!article)
-        throw error(404, 'Article not found')
+	const articles = (await git.loadData()).articles
+	const article = articles.find((article) => article.slug === params.slug)
+	if (!article) throw error(404, 'Article not found')
 
-    return {
-        article
-    }
+	// filter related articles by target
+	const target = article.metadata.target
+	const relatedArticlesByTarget = articles.filter(
+		(thisArticle) => thisArticle.metadata.target === target
+	)
+
+	// filter related articles by target and topic
+	const topics = article.metadata.topics
+	const relatedArticlesByTargetAndTopic = relatedArticlesByTarget.filter((thisArticle) =>
+		thisArticle.metadata.topics.filter((topic) => topics.includes(topic))
+	)
+
+	// maximum of 5 related articles
+	const relatedArticles =
+		relatedArticlesByTargetAndTopic.length > 5
+			? relatedArticlesByTargetAndTopic.slice(0, 5)
+			: relatedArticlesByTargetAndTopic
+
+	console.log(relatedArticles)
+	console.log(target)
+
+	return {
+		article,
+		relatedArticles
+	}
 }
