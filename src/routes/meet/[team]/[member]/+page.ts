ce98c5e9dd813a610eef32aa14777cd30069eb6d
Andrea Deluca <79919899+andrea-deluca@users.noreply.github.com>
2023-02-25 14:44:20 +0100

    auhtor meta fix

--- src/routes/meet/[team]/[member]/+page.ts
+++ src/routes/meet/[team]/[member]/+page.ts
@@ -3,7 +3,7 @@ import type { PageLoad } from './$types'
 
 export const load: PageLoad = async ({ data }) => {
 	const author = data.author
-	const component = await import(/* @vite-ignore */`../../../../assets/cms/author/${author.slug}/meta.md`)
+	const component = await import(/* @vite-ignore */`../../../../assets/cms/team/${author.slug}/meta.md`)
 	return {
 		author,
 		componentToRender: component.default,
