e89e6b47ca4767fea8d16737c295e0a1d2ecaaad
Javier Gongora <42308135+javigong@users.noreply.github.com>
2023-03-18 14:01:55 -0700

    Add timeout

--- src/routes/learn/[target]/[slug]/+page.svelte
+++ src/routes/learn/[target]/[slug]/+page.svelte
@@ -31,6 +31,11 @@
 	const description = article.metadata.meta
 	const handleShareOptions = () => {
 		openShareOptions = !openShareOptions
+		if (openShareOptions === true) {
+			setTimeout(() => {
+				openShareOptions = false
+			}, 5000);
+		}
 	}
 
 	const localtime = new Date(article.metadata.date).toLocaleDateString(undefined, {
@@ -116,10 +121,14 @@
 				</div>
 
 				<!-- Share button -->
-				<div class="flex items-center space-x-2">
-					{#if openShareOptions}
-						<div on:click={handleShareOptions} transition:fade class="flex items-center space-x-2">
-							<!-- <svg
+				<div class="static flex items-center justify-end">
+					<div class="absolute flex items-center space-x-2">
+						{#if openShareOptions}
+							<div
+								on:click={handleShareOptions}
+								class="ease-in duration-500 flex items-center space-x-2"
+							>
+								<!-- <svg
 								xmlns="http://www.w3.org/2000/svg"
 								fill="none"
 								viewBox="0 0 24 24"
@@ -134,40 +143,41 @@
 									d="M11.35 3.836c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m8.9-4.414c.376.023.75.05 1.124.08 1.131.094 1.976 1.057 1.976 2.192V16.5A2.25 2.25 0 0118 18.75h-2.25m-7.5-10.5H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V18.75m-7.5-10.5h6.375c.621 0 1.125.504 1.125 1.125v9.375m-8.25-3l1.5 1.5 3-3.75"
 								/>
 							</svg> -->
-							<LinkedIn
-								class="share-button rounded-full h-6 w-6 p-0 flex items-center justify-center"
-								{url}
-							/>
-							<Facebook
-								class="share-button rounded-full h-6 w-6 p-0 flex items-center justify-center"
-								quote={title}
-								{url}
-							/>
-							<Email
-								class="share-button rounded-full h-6 w-6 p-0 flex items-center justify-center"
-								subject={title}
-								body="{description} {url}"
-							/>
-						</div>
-					{:else}
-						<button on:click={handleShareOptions} aria-label="Share Button">
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								fill="none"
-								viewBox="0 0 24 24"
-								stroke-width="1.5"
-								stroke="currentColor"
-								class="w-6 h-6"
-							>
-								<path
-									in:draw={{ duration: 1000, easing: sineIn }}
-									stroke-linecap="round"
-									stroke-linejoin="round"
-									d="M7.217 10.907a2.25 2.25 0 100 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186l9.566-5.314m-9.566 7.5l9.566 5.314m0 0a2.25 2.25 0 103.935 2.186 2.25 2.25 0 00-3.935-2.186zm0-12.814a2.25 2.25 0 103.933-2.185 2.25 2.25 0 00-3.933 2.185z"
+								<LinkedIn
+									class="share-button rounded-full h-6 w-6 p-0 flex items-center justify-center"
+									{url}
 								/>
-							</svg>
-						</button>
-					{/if}
+								<Facebook
+									class="share-button rounded-full h-6 w-6 p-0 flex items-center justify-center"
+									quote={title}
+									{url}
+								/>
+								<Email
+									class="share-button rounded-full h-6 w-6 p-0 flex items-center justify-center"
+									subject={title}
+									body="{description} {url}"
+								/>
+							</div>
+						{:else}
+							<button on:click={handleShareOptions} aria-label="Share Button">
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									fill="none"
+									viewBox="0 0 24 24"
+									stroke-width="1.5"
+									stroke="currentColor"
+									class="w-6 h-6"
+								>
+									<path
+										in:draw={{ duration: 1000, easing: sineIn }}
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										d="M7.217 10.907a2.25 2.25 0 100 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186l9.566-5.314m-9.566 7.5l9.566 5.314m0 0a2.25 2.25 0 103.935 2.186 2.25 2.25 0 00-3.935-2.186zm0-12.814a2.25 2.25 0 103.933-2.185 2.25 2.25 0 00-3.933 2.185z"
+									/>
+								</svg>
+							</button>
+						{/if}
+					</div>
 				</div>
 			</div>
 			<!-- {@html article.content} -->
