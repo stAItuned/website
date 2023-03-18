6c016e0e2ef3e852be62e3fb36fa5636d1084a05
Javier Gongora <42308135+javigong@users.noreply.github.com>
2023-03-18 11:52:31 -0700

    Add share article to social media buttons

--- src/routes/learn/[target]/[slug]/+page.svelte
+++ src/routes/learn/[target]/[slug]/+page.svelte
@@ -1,15 +1,16 @@
 <script lang="ts">
-	import type { PageData } from './$types'
 	import { page } from '$app/stores'
-	import { draw } from 'svelte/transition'
 	import { sineIn } from 'svelte/easing'
+	import { draw, fade } from 'svelte/transition'
+	import type { PageData } from './$types'
 
 	import ArticleMetaTags from '@lib/seo/ArticleMetaTags.svelte'
 
-	import type { Article } from '@lib/interfaces'
-	import { utils } from '@lib/helpers'
-	import { PageTransition, Breadcrumb, Icons } from '@components/ui-core'
+	import { Breadcrumb, Icons, PageTransition } from '@components/ui-core'
 	import RelatedArticles from '@lib/components/features/related-articles.svelte'
+	import { utils } from '@lib/helpers'
+	import type { Article } from '@lib/interfaces'
+	import { Email, Facebook, LinkedIn } from 'svelte-share-buttons-component'
 
 	export let data: PageData
 	const article: Article = data.article
@@ -23,8 +24,14 @@
 
 	const pathname = $page.url.pathname
 
-	// allows the little animation when sharing
-	let shared = false
+	// Share article buttons
+	let openShareOptions = false
+	const url = window.location.href
+	const title = article.metadata.title
+	const description = article.metadata.meta
+	const handleShareOptions = () => {
+		openShareOptions = !openShareOptions
+	}
 
 	const localtime = new Date(article.metadata.date).toLocaleDateString(undefined, {
 		year: '2-digit',
@@ -44,14 +51,10 @@
 			// console.log('navigation.share failed, fallback')
 			try {
 				await navigator.clipboard.writeText(window.location.href)
-			} catch {
-				// console.log('not even able to copy into clipboard')
-				return
+				openShareOptions = false
+			} catch (error) {
+				console.error(error)
 			}
-			shared = true
-			setTimeout(() => {
-				shared = false
-			}, 3000)
 		}
 	}
 </script>
@@ -113,41 +116,59 @@
 				</div>
 
 				<!-- Share button -->
-				{#if shared}
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						fill="none"
-						viewBox="0 0 24 24"
-						stroke-width="1.5"
-						stroke="currentColor"
-						class="w-6 h-6"
-					>
-						<path
-							in:draw={{ duration: 500, easing: sineIn }}
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							d="M11.35 3.836c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m8.9-4.414c.376.023.75.05 1.124.08 1.131.094 1.976 1.057 1.976 2.192V16.5A2.25 2.25 0 0118 18.75h-2.25m-7.5-10.5H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V18.75m-7.5-10.5h6.375c.621 0 1.125.504 1.125 1.125v9.375m-8.25-3l1.5 1.5 3-3.75"
-						/>
-					</svg>
-				{:else}
-					<button on:click={share} aria-label="Share Button">
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							fill="none"
-							viewBox="0 0 24 24"
-							stroke-width="1.5"
-							stroke="currentColor"
-							class="w-6 h-6"
-						>
-							<path
-								in:draw={{ duration: 1000, easing: sineIn }}
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								d="M7.217 10.907a2.25 2.25 0 100 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186l9.566-5.314m-9.566 7.5l9.566 5.314m0 0a2.25 2.25 0 103.935 2.186 2.25 2.25 0 00-3.935-2.186zm0-12.814a2.25 2.25 0 103.933-2.185 2.25 2.25 0 00-3.933 2.185z"
+				<div class="flex items-center space-x-2">
+					{#if openShareOptions}
+						<div on:click={handleShareOptions} transition:fade class="flex items-center space-x-2">
+							<!-- <svg
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 24 24"
+								stroke-width="1.5"
+								stroke="currentColor"
+								class="w-6 h-6"
+							>
+								<path
+									in:draw={{ duration: 500, easing: sineIn }}
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									d="M11.35 3.836c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m8.9-4.414c.376.023.75.05 1.124.08 1.131.094 1.976 1.057 1.976 2.192V16.5A2.25 2.25 0 0118 18.75h-2.25m-7.5-10.5H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V18.75m-7.5-10.5h6.375c.621 0 1.125.504 1.125 1.125v9.375m-8.25-3l1.5 1.5 3-3.75"
+								/>
+							</svg> -->
+							<LinkedIn
+								class="share-button rounded-full h-6 w-6 p-0 flex items-center justify-center"
+								{url}
 							/>
-						</svg>
-					</button>
-				{/if}
+							<Facebook
+								class="share-button rounded-full h-6 w-6 p-0 flex items-center justify-center"
+								quote={title}
+								{url}
+							/>
+							<Email
+								class="share-button rounded-full h-6 w-6 p-0 flex items-center justify-center"
+								subject={title}
+								body="{description} {url}"
+							/>
+						</div>
+					{:else}
+						<button on:click={handleShareOptions} aria-label="Share Button">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 24 24"
+								stroke-width="1.5"
+								stroke="currentColor"
+								class="w-6 h-6"
+							>
+								<path
+									in:draw={{ duration: 1000, easing: sineIn }}
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									d="M7.217 10.907a2.25 2.25 0 100 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186l9.566-5.314m-9.566 7.5l9.566 5.314m0 0a2.25 2.25 0 103.935 2.186 2.25 2.25 0 00-3.935-2.186zm0-12.814a2.25 2.25 0 103.933-2.185 2.25 2.25 0 00-3.933 2.185z"
+								/>
+							</svg>
+						</button>
+					{/if}
+				</div>
 			</div>
 			<!-- {@html article.content} -->
 			<svelte:component this={componentToRender} />
