9fb56df9e53328204c2375473f4eb8006a4f1c75
daniele21 <danielemoltisanti@live.it>
2024-12-25 12:35:46 +0100

    update cms and relevant

--- src/lib/configs/home.ts
+++ src/lib/configs/home.ts
@@ -5,12 +5,13 @@ export const ARTICLE_CARD_COLORS = ['#ffb347', '#a7c7e7', '#47efff', '#77dd77', 
 export const AMOUNT_OF_ARTICLES = 8 as const
 
 export const RELEVANT_ARTICLES_SLUG = [
+    'imarena-ai-content-creation-guide',
+    'google-ai-studio-guide',
     'openai-o3-model-ai-advancements',
-    'how-to-handle-imbalanced-data-machine-learning',
-    'study-case-the-last-of-us',
+    'ai-translator-lara-global-communication',
+    'deep-learning-pytorch-template',
     'time-series-forecasting-with-fraction-differentiation',
-    'five-reasons-why-my-machine-learning-models-failed',
-    '30-days-of-machine-learning-engineering',
-    'ml-vs-dl',
-    'the-power-of-prompt-engineering'
+    'search-gpt-vs-google-search-comparison',
+    'notebooklm-ai-collaboration-and-comprehension',
+    'ml-vs-dl'
 ] as const 
